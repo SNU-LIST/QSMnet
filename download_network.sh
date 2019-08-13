aa0af9db470dfd5a1daf2e241994e0c601ae2d5b
wjjung93 <wjjung93@snu.ac.kr>
2019-08-13 10:17:01 +0900

    Update download_network.sh

--- download_network.sh
+++ download_network.sh
@@ -1,3 +1,16 @@
+#
+# Description :
+#   Download code of QSMnet and QSMnet+ files.
+#   Network files are located in Google drive link:
+#   https://drive.google.com/drive/u/0/folders/1E7e9thvF5Zu68Sr9Mg3DBi-o4UdhWj-8 
+#
+# Copyright @ Woojin Jung & Jaeyeon Yoon
+# Laboratory for Imaging Science and Technology
+# Seoul National University
+# email : wjjung93@snu.ac.kr
+#
+
+
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1--IZJxqbpc4WkKI4EOJG5J5OnXa7CfOh' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1--IZJxqbpc4WkKI4EOJG5J5OnXa7CfOh" -O QSMnet.tar.gz && rm -rf /tmp/cookies.txt 
 
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-1Ey1I9SNgbGnL3nnBVafKtoyaHgAfAQ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-1Ey1I9SNgbGnL3nnBVafKtoyaHgAfAQ" -O QSMnet+.tar.gz && rm -rf /tmp/cookies.txt

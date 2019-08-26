85efdf09bd36ea5cfef22f8cd01440e05dc83fc7
woojin <wjjung93@snu.ac.kr>
2019-08-26 14:48:29 +0900

    download_network.sh

--- download_network.sh
+++ download_network.sh
@@ -13,7 +13,7 @@
 
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1--IZJxqbpc4WkKI4EOJG5J5OnXa7CfOh' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1--IZJxqbpc4WkKI4EOJG5J5OnXa7CfOh" -O QSMnet.tar.gz && rm -rf /tmp/cookies.txt 
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-1Ey1I9SNgbGnL3nnBVafKtoyaHgAfAQ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-1Ey1I9SNgbGnL3nnBVafKtoyaHgAfAQ" -O QSMnet+.tar.gz && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-Fi8b1H_vxKiBw_b6fo8mXpcJ5NCiLmN' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-Fi8b1H_vxKiBw_b6fo8mXpcJ5NCiLmN" -O QSMnet+.tar.gz && rm -rf /tmp/cookies.txt
 
 tar -xvzf QSMnet.tar.gz
 tar -xvzf QSMnet+.tar.gz

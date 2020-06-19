3126d0a253720a047c1c6b3c639bf40ab499e3a6
wjjung93 <wjjung93@snu.ac.kr>
2020-06-19 17:07:17 +0900

    update

--- Checkpoints/download_network.sh
+++ Checkpoints/download_network.sh
@@ -10,8 +10,13 @@
 # email : wjjung93@snu.ac.kr
 #
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1mxayxvumshJAHNhVnD8UOem4uD8C85TY' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1mxayxvumshJAHNhVnD8UOem4uD8C85TY" -O QSMnet_64.tar.gz && rm -rf /tmp/cookies.txt  
+
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1mxxxNlcxNEQVDModRtMKl5P8CeupSkMY' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1mxxxNlcxNEQVDModRtMKl5P8CeupSkMY" -O QSMnet+_64.tar.gz && rm -rf /tmp/cookies.txt 
+
 tar -xvzf QSMnet_64.tar.gz
+
 tar -xvzf QSMnet+_64.tar.gz
+
 rm QSMnet_64.tar.gz
+
 rm QSMnet+_64.tar.gz

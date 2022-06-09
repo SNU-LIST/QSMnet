8c4bb62aeffe531e17151b2c0856ce41f6fa2a59
LISTatSNU <49898081+LISTatSNU@users.noreply.github.com>
2022-06-09 10:27:36 +0900

    Update download_network.sh
    relocate google drive storage for network

--- Checkpoints/download_network.sh
+++ Checkpoints/download_network.sh
@@ -2,16 +2,16 @@
 # Description :
 #   Download code of QSMnet and QSMnet+ files.
 #   Network files are located in Google drive link:
-#   https://drive.google.com/drive/u/0/folders/1E7e9thvF5Zu68Sr9Mg3DBi-o4UdhWj-8 
+#   https://drive.google.com/drive/folders/1fQKauCatiJezHfbre9yjqYgM1LwtwJpv
 #
 # Copyright @ Woojin Jung & Jaeyeon Yoon
 # Laboratory for Imaging Science and Technology
 # Seoul National University
-# email : wjjung93@snu.ac.kr
+# email : dhcntjr9696@snu.ac.kr
 #
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1mxayxvumshJAHNhVnD8UOem4uD8C85TY' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1mxayxvumshJAHNhVnD8UOem4uD8C85TY" -O QSMnet_64.tar.gz && rm -rf /tmp/cookies.txt  
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1fPIDxIL8gMd9mIwjyyC3xbGXlco-dWe0' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1fPIDxIL8gMd9mIwjyyC3xbGXlco-dWe0" -O QSMnet_64.tar.gz && rm -rf /tmp/cookies.txt  
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1mxxxNlcxNEQVDModRtMKl5P8CeupSkMY' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1mxxxNlcxNEQVDModRtMKl5P8CeupSkMY" -O QSMnet+_64.tar.gz && rm -rf /tmp/cookies.txt 
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1fFYW-Az6lBgyo10Km5CryLAorRrX5dou' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1fFYW-Az6lBgyo10Km5CryLAorRrX5dou" -O QSMnet+_64.tar.gz && rm -rf /tmp/cookies.txt 
 
 tar -xvzf QSMnet_64.tar.gz
 

ee261f25e0fd55deea43439aac853ac7f2f66da9
OHCHUNGSEOK <59555632+OHCHUNGSEOK@users.noreply.github.com>
2023-12-05 12:56:42 +0900

    Update download_network.sh

--- Checkpoints/download_network.sh
+++ Checkpoints/download_network.sh
@@ -11,7 +11,7 @@
 #
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1kyRKSPN78i1npIaFvJJJgmB84v-N106m' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1kyRKSPN78i1npIaFvJJJgmB84v-N106m" -O QSMnet_64.tar.gz && rm -rf /tmp/cookies.txt  
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1YeQ_kNpB1W5vok96sTNzNE7_uP8pCFyU' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1YeQ_kNpB1W5vok96sTNzNE7_uP8pCFyU" -O QSMnet+_64.tar.gz && rm -rf /tmp/cookies.txt 
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ee5nI-MMySImX2tM4m71szcjFow4DD5g' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ee5nI-MMySImX2tM4m71szcjFow4DD5g" -O QSMnet+_64.tar.gz && rm -rf /tmp/cookies.txt 
 
 tar -xvzf QSMnet_64.tar.gz
 
